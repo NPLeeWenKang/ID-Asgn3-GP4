27760ebb9d1e90d4f8b1526fe14b2e8d4b4d7f17
WenKang <wenkang11861@gmail.com>
2021-02-10 14:31:47 +0800

    added signout button

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         console.log(user.uid)
-        //window.location = "profile.html"
+        window.location = "profile.html"
     }
 });
 
